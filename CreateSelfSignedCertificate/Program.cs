a1ddd10b334170746e08bd765a808757ed7a5b3e
Tasibaev Davron <tdav@mail.ru>
2022-07-06 15:12:05 +0500

    asdasd

--- CreateSelfSignedCertificate/Program.cs
+++ CreateSelfSignedCertificate/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Security;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 
 namespace CreateSelfSignedCertificate
 {
@@ -13,163 +12,66 @@ namespace CreateSelfSignedCertificate
         {
             CreateCertificateForServerAuthentication();
 
-           // CreateCertificateForClientAuthentication();
+            // CreateCertificateForClientAuthentication();
         }
 
         private static void CreateCertificateForServerAuthentication()
         {
-            // Generate private-public key pair
             var rsaKey = RSA.Create(2048);
 
-            // Describe certificate
-            Console.WriteLine("Subject = 'CN=localhost");
             var subject_domain = Console.ReadLine();
             string subject = "CN=" + subject_domain;
+            Console.WriteLine($"Subject = '{subject}");
 
-            // Create certificate request
-            var certificateRequest = new CertificateRequest(
-                subject,
-                rsaKey,
-                HashAlgorithmName.SHA256,
-                RSASignaturePadding.Pkcs1
-            );
+            var certRequest = new CertificateRequest(subject, rsaKey, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
-            certificateRequest.CertificateExtensions.Add(
-                new X509BasicConstraintsExtension(
-                    certificateAuthority: false,
-                    hasPathLengthConstraint: false,
-                    pathLengthConstraint: 0,
-                    critical: true
-                )
-            );
-
-            certificateRequest.CertificateExtensions.Add(
-                new X509KeyUsageExtension(
-                    keyUsages:
-                        X509KeyUsageFlags.DigitalSignature
-                        | X509KeyUsageFlags.KeyEncipherment,
-                    critical: false
-                )
-            );
-
-            certificateRequest.CertificateExtensions.Add(
-                new X509SubjectKeyIdentifierExtension(
-                    key: certificateRequest.PublicKey,
-                    critical: false
-                )
-            );
-
-            certificateRequest.CertificateExtensions.Add(new X509Extension(new AsnEncodedData(subject_domain, new byte[] { 48, 11, 130, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116 }), false));
+            certRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(certificateAuthority: false, hasPathLengthConstraint: false, pathLengthConstraint: 0, critical: true));
+            certRequest.CertificateExtensions.Add(new X509KeyUsageExtension(keyUsages: X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment, critical: false));
+            certRequest.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(key: certRequest.PublicKey, critical: false));
+            certRequest.CertificateExtensions.Add(new X509Extension(new AsnEncodedData(subject_domain, new byte[] { 48, 11, 130, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116 }), false));
 
             var expireAt = DateTimeOffset.Now.AddYears(5);
+            var certificate = certRequest.CreateSelfSigned(DateTimeOffset.Now, expireAt);
 
-            var certificate = certificateRequest.CreateSelfSigned(DateTimeOffset.Now, expireAt);
+            var exportableCert = new X509Certificate2(certificate.Export(X509ContentType.Cert), (string)null, X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet).CopyWithPrivateKey(rsaKey);
+            exportableCert.FriendlyName = "My Self Certificate For Server Authorization";
 
-            // Export certificate with private key
-            var exportableCertificate = new X509Certificate2(
-                certificate.Export(X509ContentType.Cert),
-                (string)null,
-                X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet
-            ).CopyWithPrivateKey(rsaKey);
+            var sch = new SecureString();
+            foreach (var @char in "123456") { sch.AppendChar(@char); }
 
-            exportableCertificate.FriendlyName = "Ivan Yakimov Test-only Certificate For Server Authorization";
+            File.WriteAllBytes($"{subject}_ServerCert.pfx", exportableCert.Export(X509ContentType.Pfx, sch));
 
-            // Create password for certificate protection
-            var passwordForCertificateProtection = new SecureString();
-            foreach (var @char in "123456")
-            {
-                passwordForCertificateProtection.AppendChar(@char);
-            }
-
-            // Export certificate to a file.
-            File.WriteAllBytes(
-                "certificateForServerAuthorization.pfx",
-                exportableCertificate.Export(
-                    X509ContentType.Pfx,
-                    passwordForCertificateProtection
-                )
-            );
-
-            // Test correctness of export
-            var loadedCertificate = new X509Certificate2("certificateForServerAuthorization.pfx", passwordForCertificateProtection);
-
+            var loadedCertificate = new X509Certificate2($"{subject}_ServerCert.pfx", sch);
             Console.WriteLine(loadedCertificate.FriendlyName);
         }
 
         private static void CreateCertificateForClientAuthentication()
         {
-            // Generate private-public key pair
             var rsaKey = RSA.Create(2048);
 
-            // Describe certificate                
-            Console.WriteLine("Subject = 'CN=Ivan Yakimov");
-            var s = Console.ReadLine();
-            string subject = "CN=" + s;
+            var subject_domain = Console.ReadLine();
+            string subject = "CN=" + subject_domain;
+            Console.WriteLine($"Subject = '{subject}");
 
-            // Create certificate request
-            var certificateRequest = new CertificateRequest(
-                    subject,
-                    rsaKey,
-                    HashAlgorithmName.SHA256,
-                    RSASignaturePadding.Pkcs1
-                );
+            var certRequest = new CertificateRequest(subject, rsaKey, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
-            certificateRequest.CertificateExtensions.Add(
-                new X509BasicConstraintsExtension(
-                    certificateAuthority: false,
-                    hasPathLengthConstraint: false,
-                    pathLengthConstraint: 0,
-                    critical: true
-                )
-            );
-
-            certificateRequest.CertificateExtensions.Add(
-                new X509KeyUsageExtension(
-                    keyUsages:
-                        X509KeyUsageFlags.DigitalSignature
-                        | X509KeyUsageFlags.KeyEncipherment,
-                    critical: false
-                )
-            );
-
-            certificateRequest.CertificateExtensions.Add(
-                new X509SubjectKeyIdentifierExtension(
-                    key: certificateRequest.PublicKey,
-                    critical: false
-                )
-            );
+            certRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(certificateAuthority: false, hasPathLengthConstraint: false, pathLengthConstraint: 0, critical: true));
+            certRequest.CertificateExtensions.Add(new X509KeyUsageExtension(keyUsages: X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment, critical: false));
+            certRequest.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(key: certRequest.PublicKey, critical: false));
 
             var expireAt = DateTimeOffset.Now.AddYears(5);
 
-            var certificate = certificateRequest.CreateSelfSigned(DateTimeOffset.Now, expireAt);
+            var certificate = certRequest.CreateSelfSigned(DateTimeOffset.Now, expireAt);
+            var exportableCertificate = new X509Certificate2(certificate.Export(X509ContentType.Cert), (string)null, X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet).CopyWithPrivateKey(rsaKey);
 
-            // Export certificate with private key
-            var exportableCertificate = new X509Certificate2(
-                certificate.Export(X509ContentType.Cert),
-                (string)null,
-                X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet
-            ).CopyWithPrivateKey(rsaKey);
+            exportableCertificate.FriendlyName = "My Self Certificate For Server Authorization";
 
-            exportableCertificate.FriendlyName = "Ivan Yakimov Test-only Certificate For Client Authorization";
+            var sch = new SecureString();
+            foreach (var @char in "123456") { sch.AppendChar(@char); }
 
-            // Create password for certificate protection
-            var passwordForCertificateProtection = new SecureString();
-            foreach (var @char in "123456")
-            {
-                passwordForCertificateProtection.AppendChar(@char);
-            }
+            File.WriteAllBytes($"{subject}_ClientCert.pfx", exportableCertificate.Export(X509ContentType.Pfx, sch));
 
-            // Export certificate to a file.
-            File.WriteAllBytes(
-                "certificateForClientAuthorization.pfx",
-                exportableCertificate.Export(
-                    X509ContentType.Pfx,
-                    passwordForCertificateProtection
-                )
-            );
-
-            // Test correctness of export
-            var loadedCertificate = new X509Certificate2("certificateForClientAuthorization.pfx", passwordForCertificateProtection);
+            var loadedCertificate = new X509Certificate2($"{subject}_ClientCert.pfx", sch);
 
             Console.WriteLine(loadedCertificate.FriendlyName);
         }
